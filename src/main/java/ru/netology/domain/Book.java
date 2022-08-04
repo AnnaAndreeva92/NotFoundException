e2b0fabb19549cf6537ec8fc71c109803d95465d
Anna Andreeva <ann-stesha@yandex.ru>
2022-08-04 15:20:21 +0300

    NotFoundException

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,26 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+
+public class Book extends Product {
+    private String author;
+
+    public Book(int id, String title, int price, String author) {
+        super(id, title, price);
+        this.author = author;
+    }
+    @Override
+    public boolean matches(String search ) {
+        return super.matches(search) || author.equals(search);
+    }
+}
---
+++ src/main/java/ru/netology/domain/Product.java
@@ -0,0 +1,23 @@
+package ru.netology.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Product {
+    private int id;
+    private String title;
+    private int price;
+
+    public boolean matches(String search) {
+        return title.equals(search);
+    }
+}
+
